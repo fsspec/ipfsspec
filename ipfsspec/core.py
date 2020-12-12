112c75921467fd2c43e8b6d797d0d47f72a11354
Tobias Kölling <tobi@die70.de>
2020-12-12 01:09:45 +0100

    add simple buffering for open files
    Currently the whole file is requested everytime, even if only a small
    range is to be used. Many applications might request several ranges, in
    which case repeated requests should be avoided.

--- ipfsspec/core.py
+++ ipfsspec/core.py
@@ -143,8 +143,14 @@ class IPFSFileSystem(AbstractFileSystem):
 
 
 class IPFSBufferedFile(AbstractBufferedFile):
+    def __init__(self, *args, **kwargs):
+        super(IPFSBufferedFile, self).__init__(*args, **kwargs)
+        self.__content = None
+
     def _fetch_range(self, start, end):
-        content = self.fs.cat_file(self.path)[start:end]
+        if self.__content is None:
+            self.__content = self.fs.cat_file(self.path)
+        content = self.__content[start:end]
         if "b" not in self.mode:
             return content.decode("utf-8")
         else:
